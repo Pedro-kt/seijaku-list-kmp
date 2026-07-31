b93fe1218298ee209fb56491f6fdb87695cde48a
Pedro-kt <bustamante.dev.mobile@gmail.com>
2026-07-31 15:52:40 -0300

    feat: configure Coil with Ktor network fetcher and enable crossfade in image loading

--- shared/src/commonMain/kotlin/com/yumedev/seijakulistkmp/App.kt
+++ shared/src/commonMain/kotlin/com/yumedev/seijakulistkmp/App.kt
@@ -5,18 +5,33 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.SlideTransition
+import coil3.ImageLoader
+import coil3.compose.setSingletonImageLoaderFactory
+import coil3.network.ktor3.KtorNetworkFetcherFactory
+import coil3.request.crossfade
 import com.yumedev.seijakulistkmp.di.animeModule
 import com.yumedev.seijakulistkmp.di.coreModule
 import com.yumedev.seijakulistkmp.di.homeModule
+import com.yumedev.seijakulistkmp.di.networkModule
 import com.yumedev.seijakulistkmp.features.main.presentation.MainScreen
 import org.koin.compose.KoinApplication
 
 @Composable
 @Preview
 fun App() {
+    setSingletonImageLoaderFactory { context ->
+        ImageLoader.Builder(context)
+            .components {
+                add(KtorNetworkFetcherFactory())
+            }
+            .crossfade(true)
+            .build()
+    }
+
     KoinApplication(application = {
         modules(
             coreModule,
+            networkModule,
             animeModule,
             homeModule
         )
