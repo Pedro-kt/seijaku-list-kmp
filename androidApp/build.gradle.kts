3b1870e5e1b8862e515d15d45baf952ba281218e
Pedro-kt <bustamante.dev.mobile@gmail.com>
2026-09-08 22:54:28 -0300

    chore: update version code to 4 and version name to 1.1.1

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.yumedev.seijakulistkmp"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.1.1"
     }
     packaging {
         resources {
